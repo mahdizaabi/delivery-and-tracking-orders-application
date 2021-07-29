6eb654a656f153e707110ab97735f07358380b7e
mahdizaabi <mahdi.zaabi@gmail.com>
2021-07-29 18:21:40 +0200

    add home screen

--- client/src/screens/Homescreen.js
+++ client/src/screens/Homescreen.js
@@ -1,10 +1,17 @@
 import React from 'react'
 import pizzas from '../pizzasdata'
-
+import Productcard from '../components/Productcard'
 export default function Homescreen() {
-    return(
-        <div>
-            <h1>{pizzas.length}</h1>
+    const createProductList = () => pizzas.map((item,index) => {
+        return (
+            <div key={index} className="productcard__box col-md-4 p-4">
+                    <Productcard product={item} />
+            </div>
+        )
+    });
+    return (
+        <div className="row container-fluid">
+            {createProductList()}
         </div>
     )
 }
