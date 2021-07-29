0d00c78b14a6bb7e97e5c76a5012cfdf6d040648
mahdizaabi <mahdi.zaabi@gmail.com>
2021-07-29 18:22:28 +0200

    update file

--- client/src/pizzasdata.js
+++ client/src/pizzasdata.js
@@ -51,7 +51,7 @@ const pizzas = [
     ],
     "category": "veg",
     "description": "Corn over the base makes it look beautiful. It is served with tomato sauce and chili flakes are sprinkled over the topping according the taste. After mixing all the ingredients, it is baked by adding cheese and corn for added flavor to pizza. Corn adds health and sweet taste to the pizza.",
-    "image": "https://www.crazymasalafood.com/wp-content/images/golden-1.jpg"
+    "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRYG5TzdWNaIDYzZDduV3pdcQbreJEWvIYmzg&usqp=CAU"
   },
   {
     "name": "Jalapeno & Red Paprika Pizza",
@@ -68,7 +68,7 @@ const pizzas = [
       }
     ],
     "category": "veg",
-    "image": "https://www.crazymasalafood.com/wp-content/images/jalepeno.jpg",
+    "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRYG5TzdWNaIDYzZDduV3pdcQbreJEWvIYmzg&usqp=CAU",
     "description": "This pizza is amazing and can become more delicious if we will add some more cheese in it. Ingredients are yeast, sugar, olive oil, salt, and all-purpose flour in a big bowl. After mixing all the ingredients, it is baked by adding Jalapeno and Paprika with corns over the cheese layer. The base is made crunchy to give it best taste. It can be made more tasty by sprinkling chili flakes and Oregano as per the taste."
   },
   {
@@ -104,7 +104,7 @@ const pizzas = [
       }
     ],
     "category": "veg",
-    "image": "https://www.crazymasalafood.com/wp-content/images/double-1.jpg",
+    "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRmE2hzuzGoAydFaLDFFwRbFEcwREEXKcEyXw&usqp=CAU",
     "description": "This is a plain pizza which have cheese on it which is margherita and is delicious because of the loads of cheese. After mixing all the ingredients, it is baked by adding the cheese as topping over it. The base is perfectly prepared by adding double layer of cheese over it"
   }
 ]
