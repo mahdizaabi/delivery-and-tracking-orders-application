2b58582e56e9c83e8a876e2400de82fbb42034a4
mahdizaabi <mahdi.zaabi@gmail.com>
2021-07-30 03:50:55 +0200

    add conditionnal rendering for laoding and failed status

--- client/src/screens/Homescreen.js
+++ client/src/screens/Homescreen.js
@@ -1,17 +1,31 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import pizzas from '../pizzasdata'
-import Productcard from '../components/Productcard'
+import Productcard from '../components/Productcard';
+import { getAllProducts } from '../actions/productActions'
 export default function Homescreen() {
-    const createProductList = () => pizzas.map((item,index) => {
+    const dispatch = useDispatch();
+    const productsState = useSelector(state => state.getProductsReducer)
+    const { error, products, loading } = productsState;
+    useEffect(() => {
+        dispatch(getAllProducts());
+
+    }, [])
+    const createProductList = () => pizzas.map((item, index) => {
         return (
             <div key={index} className="productcard__box col-md-4 p-4">
-                    <Productcard product={item} />
+                <Productcard product={item} />
             </div>
         )
     });
     return (
         <div className="row container-fluid">
-            {createProductList()}
+            {loading ?
+                (<h1>Loading...</h1>)
+                : error ?
+                    (<h1>Something went wrong</h1>)
+                    : createProductList()}
+
         </div>
     )
 }
