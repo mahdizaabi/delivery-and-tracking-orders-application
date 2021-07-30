396289b476de8585296eb8f12a857353ac334b8d
mahdizaabi <mahdi.zaabi@gmail.com>
2021-07-30 13:47:04 +0200

    integrate the local store with the cartItems Reducer

--- client/src/store.js
+++ client/src/store.js
@@ -5,11 +5,19 @@ import thunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
 
 import {getProductsReducer} from './Reducers/productReducer'
+import {CartReducer} from './Reducers/cartReducer';
 
 const finalReducer = combineReducers({
-    getProductsReducer: getProductsReducer
+    getProductsReducer: getProductsReducer,
+    CartReducer
 })
-const insitialState= {};
+
+/* Local storage */
+const cart_Items_Local_Storage = localStorage.getItem('cartItems') ? JSON.parse(localStorage.getItem('cartItems')) : []
+
+/*               */
+const insitialState= {CartReducer:{cartItems:cart_Items_Local_Storage}};
+
 const composeEnhancer = composeWithDevTools({})
 const store = createStore(finalReducer, insitialState,composeEnhancer(applyMiddleware(thunk)))
 
