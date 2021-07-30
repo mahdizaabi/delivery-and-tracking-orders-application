7e8d78ae3bec17342a97d0c62559d2137c7403dc
mahdizaabi <mahdi.zaabi@gmail.com>
2021-07-30 03:49:47 +0200

    add Loading message

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -6,13 +6,12 @@ export default function Navbar() {
 
     return (
         <div>
-
             <nav className="navbar navbar-expand-lg navbar-light bg-light d-flex justify-content-between shadow p-3 mb-5 bg-white rounded">
                 <a className="navbar-brand" href="###">Navbar</a>
                 <div className="collapse navbar-collapse" id="navbarNav">
                     <ul className="navbar-nav">
                         <li className="nav-item active">
-                            <a className="nav-link" href="###">Home <span className="sr-only">Login</span></a>
+                            <a className="nav-link" href="###">Home<span className="sr-only">Login</span></a>
                         </li>
                         <li className="nav-item">
                             <a className="nav-link" href="###">Cart</a>
