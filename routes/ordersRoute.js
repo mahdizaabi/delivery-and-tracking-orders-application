5cef7b0010a079b59677ce9b4e51af9b2b7461e3
mahdizaabi <mahdi.zaabi@gmail.com>
2021-08-05 00:24:32 +0200

    real time tracking system

--- routes/ordersRoute.js
+++ routes/ordersRoute.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const orderController = require('../controllers/ordersController');
 
 router.post("/placeorder", orderController.placeOrder);
+router.get("/getuserorders", orderController.getAllUserOrder);
 
 module.exports = router;
