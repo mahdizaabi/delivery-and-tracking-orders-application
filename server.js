d7f533a6cc43ffb39a3bd6ecf2308a9f245e3dd7
mahdizaabi <mahdi.zaabi@gmail.com>
2021-08-05 00:23:23 +0200

    update server configuration

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ const productsRoute = require('./routes/productsRoute');
 const userRegistrationRoute = require('./routes/userRoute');
 const orderRoute = require('./routes/ordersRoute');
 
+const getUserOrders = require('./routes/ordersRoute');
 
 app.use('/api/products', productsRoute);
 app.use('/api/users', userRegistrationRoute);
@@ -21,7 +22,7 @@ app.get('/', function (req, res, next) {
     res.send("welcome to Pizza delvery application!")
 })
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5001;
 
 app.listen(port, () => 'server running on port 5000')
 
